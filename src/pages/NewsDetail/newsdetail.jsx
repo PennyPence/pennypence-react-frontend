073268b332d14728c37bd6bc2634c3f99e5b302b
choikeunyoung <ought119@gmail.com>
2023-02-17 23:08:06 +0900

    feat : 뉴스 디테일 페이지 만들기
    뉴스 디테일 페이지 탭 메뉴 구현했다.
    Close #13

---
+++ src/pages/NewsDetail/newsdetail.jsx
@@ -0,0 +1,122 @@
+import style from "./newsdetail.module.css"
+import MakeStar from "../../hoc/makestar/makestar"
+import NavBar from "../../components/navbar"
+import testImage from "../../assets/images/logo.png"
+import { useState } from "react"
+
+function NewsDetailPage() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const tabContentArr = [
+        {
+            tabTitle : (
+                <li onClick={() => tabHandleer(0)} className={activeIndex===0 ? style[`tab-menu__active`] : style[`tab-menu`]}> 최신순 </li>
+            ),
+            tabContent : (
+                <>
+                <span className={style[`tab-menu__page__title`]}> 새로운 소식! </span>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                </>
+            )
+        },
+        {
+            tabTitle:(
+                <li onClick={() => tabHandleer(1)} className={activeIndex===1 ? style[`tab-menu__active`] : style[`tab-menu`]}> 일자순 </li>
+            ),
+            tabContent:(
+                <>
+                <span className={style[`tab-menu__page__title`]}> 새로운 소식! </span>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                <div className={style[`tab-menu__page`]}>
+                    <div className={style[`tab-menu__page__box`]}>
+                        <div className={style[`tab-menu__page__box__texts`]}>
+                            <span className={style[`tab-menu__page__box__texts__title`]}>석유폭등</span>
+                            <span className={style[`tab-menu__page__box__texts__content`]}>블라블라 뉴스 내용 칸입니다.</span>
+                        </div>
+                        <img className={style[`tab-menu__page__box__image`]} src={testImage} alt="" />
+                    </div>
+                </div>
+                </>
+            )
+        }
+    ];
+
+    const tabHandleer = (index) => {
+        setActiveIndex(index)
+    }
+
+    return (
+        <section className={style[`detail-page`]}>
+        <MakeStar />
+        <ul className={style.tabs}>
+            {tabContentArr.map((section, index)=>{
+                return section.tabTitle
+            })}
+        </ul>
+        { tabContentArr[activeIndex].tabContent };
+        <NavBar />
+        </section>
+    );
+}
+
+export default NewsDetailPage
